a7618a49d4e2108311f75a13153d4d2111cad0d3
Dokueki <lolmanoo20@gmail.com>
2022-04-01 14:33:30 +0600

    Added Shambles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed;
+
+
+    
+    void Update()
+    {
+        
+    }
+}
